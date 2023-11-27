f6ef58466f99fa89e2f5a4447d4d7c028fffd5f9
juanestarli <104745448+juanestarli@users.noreply.github.com>
2023-11-27 19:09:48 -0300

    empecé el main

---
+++ ADOO/src/test/MainTest.java
@@ -0,0 +1,27 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    //Crear un socio
+
+    //loguearse como socio
+
+    //Registrar medicion
+
+    //elegir un objetivo
+
+    //obtener trofeo alguno
+
+    //completar entrenamiento
+
+    //Reforzar un ejercicio
+
+    //Registrar los pesos y datos del ejercicio
+
+    //completar rutina
+
+
+
+}
