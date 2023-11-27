0d48241226efc8ad76706bc4bd95d0bf134db59b
juanestarli <104745448+juanestarli@users.noreply.github.com>
2023-11-26 23:38:25 -0300

    sume unos unitTests

--- ADOO/src/test/UnitTests.java
+++ ADOO/src/test/UnitTests.java
@@ -9,4 +9,29 @@ public class UnitTests {
         //Testa aca
     }
 
+    @Test
+    public void socioRegistraMedicion(){
+        //Test aca
+    }
+
+    @Test
+    public void socioIngresaDatosEjercicio(){
+        //socio ingresa datos del ejercicio que realizo
+    }
+
+    @Test
+    public void socioMarcaEntrenamientoCompleto(){
+        //Socio marca que completo el entrenamiento
+    }
+
+    @Test
+    public void socioLogIn(){
+        //socio se loguee con datos almacenados
+    }
+
+    @Test
+    public void socioRecibeTrofeo(){
+        //que sea cualquier trofeo y que lo otorgue
+    }
+
 }
